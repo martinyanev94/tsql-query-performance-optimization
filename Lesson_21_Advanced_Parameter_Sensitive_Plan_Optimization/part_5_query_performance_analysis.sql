f8030baf7521b5a045863263123fb97590536d9a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-09 06:09:38 -0500

    Add Lesson_21_Advanced_Parameter_Sensitive_Plan_Optimization/part_5_query_performance_analysis.sql

---
+++ Lesson_21_Advanced_Parameter_Sensitive_Plan_Optimization/part_5_query_performance_analysis.sql
@@ -0,0 +1,7 @@
+SELECT qs.execution_count,
+       qs.total_worker_time,
+       qs.total_elapsed_time,
+       qp.query_plan
+FROM sys.dm_exec_query_stats AS qs
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) AS qp
+ORDER BY qs.total_worker_time DESC;
