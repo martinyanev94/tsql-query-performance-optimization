b6490381461d66ad4881e7af682a6bd86bda67d9
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-09 06:11:08 -0500

    Add Lesson_30_Understanding_Parameter-Sensitive_Plan_Optimization/part_7_GetSalesByProduct.sql

---
+++ Lesson_30_Understanding_Parameter-Sensitive_Plan_Optimization/part_7_GetSalesByProduct.sql
@@ -0,0 +1,3 @@
+EXEC GetSalesByProduct @ProductID = 5; -- Typical product
+EXEC GetSalesByProduct @ProductID = 20; -- Seasonal product
+EXEC GetSalesByProduct @ProductID = 50; -- Rarely sold product
