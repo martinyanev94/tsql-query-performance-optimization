c687ab685007d4a1d5a7fb85b5e4424a2ffaadd1
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-09 06:08:07 -0500

    Add Lesson_12_Parameter_Sensitive_Plan_Optimization_Techniques/part_5_query_cache_inspection.sql

---
+++ Lesson_12_Parameter_Sensitive_Plan_Optimization_Techniques/part_5_query_cache_inspection.sql
@@ -0,0 +1,10 @@
+SELECT 
+    cp.plan_handle, 
+    st.text AS [SQL Text], 
+    qp.query_plan 
+FROM 
+    sys.dm_exec_cached_plans AS cp 
+CROSS APPLY 
+    sys.dm_exec_sql_text(cp.plan_handle) AS st 
+CROSS APPLY 
+    sys.dm_exec_query_plan(cp.plan_handle) AS qp;
